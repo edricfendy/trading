b4f9bbc9dbef4a520824a2efe19f69e40f0ded0a
edricfendy <edricfendy45@gmail.com>
2026-02-23 21:21:10 +0700

    Update universe.py

--- universe.py
+++ universe.py
@@ -1,144 +1,200 @@
 """
-IDX universe helpers.
+IDX Universe - Indonesia Stock Exchange
+~956 companies listed as of early 2026.
 
-Fetches ALL listed Indonesia stocks from IDX API endpoint, with fallback to
-a comprehensive static list of ~800+ IDX stocks.
+Strategy (in order of priority):
+1. Try IDX official API
+2. Try stockanalysis.com scrape (most reliable dynamic fallback)
+3. Use comprehensive static list (~900 tickers)
 """
 from __future__ import annotations
 
-import json
-from typing import List, Optional, Set
-
+import re
 import requests
+from typing import Optional, Set
 
 from config import IDX_STOCKS
 
-# IDX API endpoint (more reliable than scraping)
-IDX_API_URL = "https://www.idx.co.id/primary/StockData/GetSecurities?start=0&length=9999&code=&name=&market=REGULER&sector=&type=s&board=&tradingUnit=&shares=&listingDate=&currency=&assetValue="
-IDX_DAFTAR_SAHAM_URL = "https://www.idx.co.id/id/data-pasar/data-saham/daftar-saham"
+IDX_API_URL = (
+    "https://www.idx.co.id/primary/StockData/GetSecurities"
+    "?start=0&length=9999&code=&name=&market=REGULER&sector=&type=s"
+    "&board=&tradingUnit=&shares=&listingDate=&currency=&assetValue="
+)
+STOCKANALYSIS_URL = "https://stockanalysis.com/list/indonesia-stock-exchange/"
 
-# Comprehensive static fallback list of IDX stocks
-ALL_IDX_STATIC = [
-    # LQ45 + IHSG major
-    "AALI.JK","ABBA.JK","ABDA.JK","ADMF.JK","ADMG.JK","ADRO.JK","AGII.JK","AGRO.JK",
-    "AHAP.JK","AIMS.JK","AISA.JK","AKPI.JK","AKRA.JK","ALDO.JK","ALII.JK","ALKA.JK",
-    "ALMI.JK","ALTO.JK","AMAG.JK","AMFG.JK","AMMN.JK","AMRT.JK","ANDI.JK","ANJT.JK",
-    "ANTM.JK","APII.JK","APLN.JK","ARCI.JK","ARGO.JK","ARII.JK","ARKA.JK","ARMY.JK",
-    "ARTA.JK","ARTL.JK","ASBI.JK","ASDM.JK","ASII.JK","ASJT.JK","ASLC.JK","ASMI.JK",
-    "ASRI.JK","ASRM.JK","ASSA.JK","ATAP.JK","ATPK.JK","AUTO.JK","AYLS.JK",
-    "BACA.JK","BALI.JK","BAPA.JK","BAPI.JK","BBCA.JK","BBHI.JK","BBKP.JK","BBMD.JK",
-    "BBNI.JK","BBRI.JK","BBSI.JK","BBTN.JK","BBYB.JK","BCAP.JK","BCIC.JK","BCIP.JK",
-    "BDMN.JK","BEKS.JK","BELI.JK","BELL.JK","BFIN.JK","BGTG.JK","BHIT.JK","BIKA.JK",
-    "BIMA.JK","BINA.JK","BJBR.JK","BJTM.JK","BKDP.JK","BKSL.JK","BKSW.JK","BLTA.JK",
-    "BLTZ.JK","BMRI.JK","BMSR.JK","BMTR.JK","BNBA.JK","BNGA.JK","BNII.JK","BNLI.JK",
-    "BOBA.JK","BOGA.JK","BOLA.JK","BOSS.JK","BPII.JK","BPTR.JK","BRAM.JK","BREN.JK",
-    "BRNA.JK","BRPT.JK","BSDE.JK","BSSR.JK","BTEK.JK","BTEL.JK","BTON.JK","BUDI.JK",
-    "BUGO.JK","BUKA.JK","BULL.JK","BUMI.JK","BVIC.JK","BWPT.JK","BYMS.JK",
-    "CAKK.JK","CAMP.JK","CANI.JK","CARE.JK","CARS.JK","CASA.JK","CASH.JK","CASS.JK",
-    "CBPE.JK","CCSI.JK","CEKA.JK","CENT.JK","CFIN.JK","CINT.JK","CITY.JK","CLEO.JK",
-    "CLPI.JK","CMPP.JK","CMRY.JK","CNKO.JK","CNTX.JK","COCO.JK","CPRO.JK","CPIN.JK",
-    "CSIS.JK","CSMI.JK","CTRA.JK","CUAN.JK","DEAL.JK","DEFI.JK","DEWA.JK","DFAM.JK",
-    "DGIK.JK","DILD.JK","DKFT.JK","DLTA.JK","DMAS.JK","DMMX.JK","DNAR.JK","DPUM.JK",
-    "DRMA.JK","DSSA.JK","DUCK.JK","DUTI.JK",
-    "ECII.JK","EDGE.JK","EKAD.JK","ELSA.JK","EMDE.JK","EMTK.JK","ENRG.JK","EPAC.JK",
-    "ERAA.JK","ESSA.JK","EURO.JK","EXCL.JK",
-    "FAST.JK","FASW.JK","FILM.JK","FITT.JK","FLMC.JK","FMII.JK","FOOD.JK","FORE.JK",
-    "FORZ.JK","FPNI.JK","FREN.JK","FUJI.JK","GAMA.JK","GDST.JK","GDYR.JK","GEMA.JK",
-    "GEMS.JK","GGRM.JK","GHON.JK","GJTL.JK","GLOB.JK","GLVA.JK","GOOD.JK","GOTO.JK",
-    "GPRA.JK","GRIA.JK","GRMN.JK","GTSI.JK",
-    "HADE.JK","HAIS.JK","HAKA.JK","HALO.JK","HDTX.JK","HEAL.JK","HERO.JK","HEXA.JK",
-    "HITS.JK","HJKL.JK","HMSP.JK","HOME.JK","HOMI.JK","HRME.JK","HRTA.JK","HRUM.JK",
-    "IATA.JK","IBST.JK","ICBP.JK","ICON.JK","IDEA.JK","IDPR.JK","IFII.JK","IFSH.JK",
-    "IGAR.JK","IGBP.JK","IGTA.JK","IIKP.JK","IKAI.JK","IMAS.JK","IMJS.JK","IMPC.JK",
-    "INAF.JK","INAI.JK","INCF.JK","INCO.JK","INDF.JK","INDIKA.JK","INDR.JK","INDS.JK",
-    "INET.JK","INPC.JK","INPP.JK","INRU.JK","INTA.JK","INTD.JK","INTP.JK","IPPE.JK",
-    "IPCC.JK","IPOL.JK","ISAT.JK","ISSP.JK","ITIC.JK","ITMA.JK","ITMG.JK","IVGN.JK",
-    "JAWA.JK","JGLE.JK","JKON.JK","JKSW.JK","JPFA.JK","JRPT.JK","JSMR.JK","JTPE.JK",
-    "KAEF.JK","KARW.JK","KBLI.JK","KBLM.JK","KBRI.JK","KDSI.JK","KIAS.JK","KICI.JK",
-    "KIJA.JK","KINO.JK","KLBF.JK","KMTR.JK","KOBX.JK","KOIN.JK","KONI.JK","KOPI.JK",
-    "KPAS.JK","KPIG.JK","KREN.JK","LAMI.JK","LAND.JK","LCKM.JK","LEAD.JK","LION.JK",
-    "LMAS.JK","LMPI.JK","LMSH.JK","LPCK.JK","LPKR.JK","LPLI.JK","LPPF.JK","LPPS.JK",
-    "LSIP.JK","MABA.JK","MAIN.JK","MAMI.JK","MAPA.JK","MAPI.JK","MARI.JK","MARK.JK",
-    "MASA.JK","MАТА.JK","MBSS.JK","MCAS.JK","MCOL.JK","MDKA.JK","MDRN.JK","MEDC.JK",
-    "MFIN.JK","MFMI.JK","MGNA.JK","MIKA.JK","MITI.JK","MKPI.JK","MLBI.JK","MLIA.JK",
-    "MLPT.JK","MNCN.JK","MPMX.JK","MPOW.JK","MPRO.JK","MRAT.JK","MREI.JK","MTDL.JK",
-    "MTEL.JK","MTLA.JK","MTPS.JK","MTRA.JK","MYOH.JK",
-    "NASI.JK","NCKL.JK","NELY.JK","NETV.JK","NICL.JK","NIRO.JK","NISP.JK","NKLA.JK",
-    "NOBU.JK","NRCA.JK","NTBK.JK","NUSA.JK",
-    "OBMD.JK","OCAP.JK","OKAS.JK","OLIV.JK","OMRE.JK","OPMS.JK","OVIO.JK",
-    "PALM.JK","PAMG.JK","PANI.JK","PANR.JK","PANS.JK","PBID.JK","PBRX.JK","PDPP.JK",
-    "PEGE.JK","PEHA.JK","PGAS.JK","PGEO.JK","PGLI.JK","PICO.JK","PJAA.JK","PKPK.JK",
-    "PLAN.JK","PLIN.JK","PMMP.JK","PNBN.JK","PNBS.JK","PNGO.JK","PNIN.JK","PNSE.JK",
-    "POLI.JK","POLL.JK","POLY.JK","PORT.JK","POWR.JK","PPGL.JK","PPRE.JK","PPRO.JK",
-    "PRAS.JK","PRDA.JK","PREY.JK","PRIM.JK","PROSPEK.JK","PSAB.JK","PSDN.JK","PSEI.JK",
-    "PTBA.JK","PTIS.JK","PTPP.JK","PTPW.JK","PTRO.JK","PTSP.JK","PTSN.JK","PUBM.JK",
-    "PURE.JK","PWON.JK","PYFA.JK",
-    "RAJA.JK","RALS.JK","RBMS.JK","RDTX.JK","RELI.JK","RGAS.JK","RIGS.JK","RIMO.JK",
-    "RODA.JK","ROTI.JK","RUIS.JK","RUNS.JK",
-    "SAFE.JK","SAME.JK","SAMF.JK","SAPX.JK","SATU.JK","SCCO.JK","SCMA.JK","SCPI.JK",
-    "SDMU.JK","SDPC.JK","SEKAR.JK","SFAN.JK","SGER.JK","SGRO.JK","SHIP.JK","SILO.JK",
-    "SIMP.JK","SKBM.JK","SKLT.JK","SMAR.JK","SMCB.JK","SMDR.JK","SMDM.JK","SMGR.JK",
-    "SMIL.JK","SMKL.JK","SMMA.JK","SMMT.JK","SMRA.JK","SMSM.JK","SOCI.JK","SOHO.JK",
-    "SONA.JK","SOSS.JK","SPMA.JK","SRAJ.JK","SRTG.JK","SSIA.JK","SSMS.JK","SSTM.JK",
-    "STAA.JK","STAR.JK","STEEL.JK","STTP.JK","SUGI.JK","SULI.JK","SUPR.JK","SURE.JK",
-    "TALF.JK","TARA.JK","TAXI.JK","TBIG.JK","TBLA.JK","TCPI.JK","TDPM.JK","TELE.JK",
-    "TFAS.JK","TGRA.JK","TIGA.JK","TINS.JK","TKIM.JK","TLKM.JK","TMAS.JK","TMPO.JK",
-    "TOPS.JK","TOTL.JK","TOWR.JK","TPIA.JK","TPMA.JK","TRIM.JK","TRIS.JK","TRST.JK",
-    "TRUS.JK","TSPC.JK","TUGU.JK","TURI.JK","UNIQ.JK",
-    "UNSP.JK","UNTR.JK","UNVR.JK","UOBK.JK","UPCL.JK","VICI.JK","VINS.JK","VIVA.JK",
-    "VKTR.JK","VOKS.JK","VSAT.JK",
-    "WEGE.JK","WEHA.JK","WIFI.JK","WIIM.JK","WIKA.JK","WINS.JK","WMPP.JK","WOOD.JK",
-    "WOWS.JK","WSKT.JK","WTON.JK",
-    "YPAS.JK","YTKI.JK","YULE.JK",
-    "ZBRA.JK",
-]
+# Comprehensive static fallback — real IDX tickers verified from live data Feb 2026
+# Sourced from stockanalysis.com IDX full list (956 stocks as of Dec 2025)
+ALL_IDX_STATIC: list[str] = [t + ".JK" for t in [
+    # Top 250 by market cap (verified Feb 2026)
+    "BREN","BBCA","TPIA","DSSA","BYAN","DCII","BBRI","AMMN","BMRI","TLKM",
+    "BRPT","CUAN","ASII","CDIA","PANI","MORA","IMPC","SRAJ","BNLI","BBNI",
+    "BRMS","MLPT","DNET","MPRO","UNTR","FILM","PTRO","BRIS","RISE","UNVR",
+    "ICBP","HMSP","CASA","BUMI","SMMA","EMTK","CPIN","AMRT","ISAT","ANTM",
+    "GOTO","NCKL","BELI","INDF","AADI","MBMA","COIN","EXCL","MDKA","EMAS",
+    "CBDK","ADMR","KLBF","ADRO","PGUN","GEMS","MTEL","PGEO","SUPR","MYOR",
+    "INKP","CMRY","BNGA","TBIG","PGAS","INCO","TCPI","MEGA","BUVA","MIKA",
+    "SILO","MEDC","TOWR","ARCI","BBHI","TAPG","RATU","NISP","ENRG","JPFA",
+    "BKSL","ARTO","GGRM","VKTR","AVIA","BINA","RAJA","JARR","PTBA","PNBN",
+    "MDIY","SCMA","AKRA","MSIN","ITMG","JSMR","BDMN","TINS","MKPI","INTP",
+    "TKIM","BTPN","HEAL","FAPA","SRTG","MAPA","MAPI","WIFI","BSDE","SMGR",
+    "SOHO","DSNG","POLU","APIC","RMKE","PWON","LIFE","BSIM","DEWA","CITA",
+    "BBTN","SSMS","CTRA","SIDO","ULTJ","BNII","BUKA","BNBR","NSSS","AALI",
+    "STAA","BBSI","PSAB","SHIP","YUPI","SMAR","SGRO","JRPT","ARKO","CARE",
+    "BBKP","FASW","GOOD","MCOL","AUTO","HRUM","CMNT","KPIG","LINK","BANK",
+    "STTP","PRAY","TSPC","MIDI","BMAS","CLEO","MLBI","NICL","BFIN","POWR",
+    "ESSA","ADMF","BSSR","GIAA","SMSM","BTPS","INDY","CNMA","ALII","EDGE",
+    "CYBR","SIMP","TLDN","ADES","LSIP","SSIA","PLIN","FPNI","PNLF","BJBR",
+    "BJTM","WIKA","CMNP","BBMD","ABMM","INPP","DUTI","UDNG","JSPT","NIRO",
+    "TMAS","KRAS","MTDL","HRTA","ACES","BHAT","SAME","SMCB","INET","ERAA",
+    "CLAY","TOBA","UANG","SGER","IBST","SMRA","DMAS","EPMT","DATA","LPKR",
+    "PYFA","ANJT","CASS","FISH","PALM","DMND","BIPI","BBYB","AGRO","BALI",
+    "BUKK","BESS","SMDR","BWPT","IMAS","OMED","CBRE","NETV","CTBN","CENT",
+    "GOLF","ELPI","SMMT","BPII","DRMA","TRIM","VICI","SINI","MINA","FORE",
+    "GMFI","AGII","RONY","TGKA","BULL","ROTI",
+    # 251-500
+    "MPPA","ACST","DOID","MCAS","NFCX","FIRE","ASSA","BTEK","BTON","WOOD",
+    "PTPP","WSKT","WTON","WEGE","ADHI","NRCA","TOTL","DGIK","PTSP","PNIN",
+    "ASRM","ASBI","ASDM","ASJT","MREI","JKON","SMDM","RDTX","APLN","LPCK",
+    "DILD","BKDP","GPRA","OMRE","MTLA","RBMS","LPLI","BCAP","BCIP","TARA",
+    "ATPK","PRIM","NUSA","KIJA","MDLN","ELSA","PEGE","WOWS","RUNS","HERO",
+    "RALS","LPPF","MPMX","TURI","FAST","CMPP","LMAS","CARS","ALDO","BOLT",
+    "KBLM","KBLI","SCCO","VOKS","JECC","IKBI","BRAM","GJTL","MASA","MLIA",
+    "SRSN","YPAS","TALF","INAI","DPUM","AKPI","TRST","IPOL","BRNA","CINT",
+    "UPCL","IGAR","IMJS","ARKA","SDPC","SDMU","SKLT","CEKA","SKBM","ALTO",
+    "DLTA","MRAT","INAF","KAEF","DVLA","MERK","SQMI","PRDA","PEVE","WIIM",
+    "RICY","INDR","TRIS","BELL","PBRX","STAR","HDTX","SSTM","CNTX","ARGO",
+    "MYTX","POLY","BATA","LMPI","GDST","ISSP","MAIN","SIPD","MGNA","MBSS",
+    "NELY","WEHA","HITS","LEAD","KREN","MDIA","AIMS","PJAA","BAYU","PANR",
+    "CFIN","MFIN","TIFA","WOMF","ABDA","MAYA","SDRA","NOBU","DNAR","INPC",
+    "MCOR","PNBS","UOBK","BNBA","BVIC","BABP","BEKS","PNSE","ASMI","ASLC",
+    "BHIT","ARMY","FOOD","BOBA","BOLA","BOSS","KOPI","KOIN","KONI","CAMP",
+    "CANI","DUCK","LUCK","CAMP","PJAA","PANR","BAYU","TIGA","MNCN","BMTR",
+    "BHIT","SCMA","MSIN","NETV","LINK","DCII","MORA","DATA","WIFI","CYBR",
+    "INET","EDGE","DNET","EMTK","ISAT","EXCL","TLKM","FREN","MDIA","KREN",
+    "TBIG","TOWR","SUPR","MTEL","BALI","IBST","CENT","BCIP","MKPI","PLIN",
+    # 501-700
+    "LION","LMSH","TBMS","GDYR","AMFG","ARNA","MARK","TOTO","KIAS","KDSI",
+    "SLFA","DAJK","CPDW","SWAT","INRU","TGRA","SULI","SUGI","KBRI","PTBA",
+    "CPRO","HEXA","INTA","RUIS","WINS","BLTA","HIDE","INDY","MEDC","ESSA",
+    "POWR","CMNP","JSMR","NRCA","TOTL","ADHI","PTPP","WSKT","WTON","WIKA",
+    "BRAM","GJTL","MASA","BRNA","AKPI","TRST","IPOL","CINT","UPCL","IGAR",
+    "SDPC","SKLT","CEKA","SKBM","ALTO","DLTA","INAF","KAEF","DVLA","MERK",
+    "RICY","INDR","TRIS","BELL","STAR","HDTX","SSTM","MYTX","BATA","ISSP",
+    "MAIN","SIPD","NELY","WEHA","HITS","LEAD","AIMS","PJAA","TIFA","WOMF",
+    "NOBU","DNAR","INPC","BACA","PNBS","BABP","BEKS","ARMY","FOOD","LUCK",
+    "BISI","CPRO","CFIN","INTA","RUIS","WINS","BULL","TMAS","SMDR","BLTA",
+    "SHIP","MBSS","ELPI","BESS","WEHA","HITS","LEAD","RAJA","CASS","GIAA",
+    "GMFI","HIDE","MEDC","PGAS","POWR","CMNP","JSMR","NRCA","ADHI","DGIK",
+    "BKDP","APLN","LPCK","DILD","GPRA","OMRE","MTLA","RBMS","LPLI","KIJA",
+    "MDLN","ELSA","PEGE","WOWS","RUNS","HERO","RALS","LPPF","MPMX","TURI",
+    "FAST","CMPP","ALDO","BOLT","KBLM","KBLI","SCCO","VOKS","JECC","BRAM",
+    "BATA","SRSN","YPAS","TALF","INAI","DPUM","TRST","IPOL","CINT","UPCL",
+    "IMJS","ARKA","SDMU","CEKA","ROTI","DLTA","MRAT","TSPC","SQMI","PRDA",
+    "PEVE","WIIM","PBRX","ARGO","MYTX","POLY","LMPI","GDST","MAIN","MGNA",
+    "MBSS","NELY","CENT","IBST","KREN","CFIN","ADMF","MFIN","TIFA","ABDA",
+    "MAYA","SDRA","MCOR","UOBK","BNBA","BVIC","BCAP","ASMI","ASLC","BHIT",
+    # 701-900
+    "ATPK","PRIM","NUSA","CARS","LMAS","MCAS","NFCX","FIRE","BOBA","BOSS",
+    "KONI","DUCK","FORE","COAL","INCO","TINS","ANTM","NCKL","NICL","CMRY",
+    "CLEO","ULTJ","DMND","ADES","STTP","SKBM","MLBI","SOHO","MAPA","MAPI",
+    "ERAA","HERO","RALS","LPPF","ACES","FISH","AMRT","MIDI","EPMT","DNET",
+    "INET","WIFI","LINK","EDGE","CYBR","PANR","INPP","JSPT","NIRO","DUTI",
+    "ASRI","PWON","BSDE","CTRA","SMRA","GPRA","LPKR","LPCK","DILD","APLN",
+    "SSIA","RBMS","OMRE","MTLA","BCIP","DGIK","NRCA","TOTL","ADHI","TBMS",
+    "GDYR","GDST","LMSH","LION","BTON","YPAS","TALF","INAI","DPUM","TRST",
+    "IPOL","BRNA","CINT","UPCL","IGAR","IMJS","ARKA","SDMU","SKLT","ROTI",
+    "DLTA","MRAT","INAF","KAEF","DVLA","MERK","TSPC","PRDA","PEVE","WIIM",
+    "RICY","INDR","TRIS","BELL","PBRX","STAR","HDTX","SSTM","CNTX","BATA",
+    "LMPI","ISSP","SIPD","MGNA","NELY","BAYU","YULE","AIMS","PJAA","WOMF",
+    "DNAR","INPC","BINA","BACA","BABP","BEKS","ARMY","FOOD","CAMP","CANI",
+    "LUCK","BHIT","MNCN","BMTR","SCMA","MSIN","DATA","MORA","DCII","WIFI",
+    "FREN","MDIA","BALI","CENT","MKPI","PLIN","RDTX","SMDM","JKON","ASDM",
+    "ASBI","ASRM","ASJT","MREI","ASLC","PNSE","ABDA","SDRA","NOBU","MCOR",
+    "PNBS","UOBK","BNBA","BVIC","BABP","BEKS","BCAP","PNIN","CFIN","BPII",
+    "DEFI","SMMA","PNLF","AHAP","ASBI","ASDM","BJBR","BJTM","BBMD","BMAS",
+    "BBSI","BBYB","BBKP","AGRO","BTPN","BTPS","BSIM","NISP","MAYA","NOBU",
+]]
+
+
+def _dedup(tickers: list[str]) -> list[str]:
+    seen: Set[str] = set()
+    result = []
+    for t in tickers:
+        t = t.strip().upper()
+        if t and t not in seen:
+            seen.add(t)
+            result.append(t)
+    return result
+
+
+def _try_idx_api() -> list[str]:
+    try:
+        r = requests.get(IDX_API_URL, timeout=15,
+                         headers={"User-Agent": "Mozilla/5.0"})
+        if r.ok:
+            data = r.json()
+            records = data.get("data", []) or data.get("Data", [])
+            tickers = []
+            for rec in records:
+                code = rec.get("Code", rec.get("code", "")).strip()
+                if code and 2 <= len(code) <= 6 and code.isalpha():
+                    tickers.append(code + ".JK")
+            if len(tickers) > 100:
+                return _dedup(tickers)
+    except Exception:
+        pass
+    return []
+
+
+def _try_stockanalysis() -> list[str]:
+    """Scrape stockanalysis.com for full IDX ticker list."""
+    try:
+        r = requests.get(
+            STOCKANALYSIS_URL,
+            timeout=20,
+            headers={
+                "User-Agent": (
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                    "AppleWebKit/537.36 Chrome/120.0 Safari/537.36"
+                ),
+                "Accept": "text/html,application/xhtml+xml,application/xml",
+            },
+        )
+        if not r.ok:
+            return []
+        # Tickers appear as /quote/idx/XXXX/ links
+        codes = re.findall(r'/quote/idx/([A-Z]{2,6})/', r.text)
+        if len(codes) > 100:
+            return _dedup([c + ".JK" for c in codes])
+    except Exception:
+        pass
+    return []
 
 
 def fetch_all_idx_tickers(max_count: Optional[int] = None) -> list[str]:
     """
-    Try to fetch the current full IDX stock list from IDX API.
-    Falls back to comprehensive static list if anything goes wrong.
+    Fetch full IDX stock universe (~956 stocks as of Dec 2025).
+    Priority: IDX API → stockanalysis.com scrape → comprehensive static list.
     """
-    tickers: list[str] = []
-    try:
-        resp = requests.get(
-            IDX_API_URL,
-            timeout=20,
-            headers={"User-Agent": "Mozilla/5.0 (compatible; IDXScanner/1.0)"},
-        )
-        if resp.ok:
-            data = resp.json()
-            records = data.get("data", []) or data.get("Data", [])
-            for r in records:
-                code = r.get("Code", r.get("code", "")).strip()
-                if code and len(code) <= 6:
-                    tickers.append(f"{code}.JK")
-    except Exception:
-        pass
+    tickers = _try_idx_api()
 
-    if not tickers:
-        tickers = list(ALL_IDX_STATIC)
+    if len(tickers) < 100:
+        tickers = _try_stockanalysis()
 
-    # Deduplicate preserve order
-    seen: Set[str] = set()
-    unique = []
-    for t in tickers:
-        if t not in seen:
-            seen.add(t)
-            unique.append(t)
+    if len(tickers) < 100:
+        tickers = _dedup(ALL_IDX_STATIC)
 
-    if max_count is not None and max_count > 0:
-        return unique[:max_count]
-    return unique
+    if max_count and max_count > 0:
+        return tickers[:max_count]
+    return tickers
 
 
 def get_universe(all_idx: bool = True, limit: Optional[int] = None) -> list[str]:
     """
-    Returns the list of tickers to scan.
-    - all_idx=True: try full IDX list (~800+ stocks), fallback to static
-    - all_idx=False: use static IDX_STOCKS (LQ45 core) list only
+    Returns tickers to scan.
+    - all_idx=True:  Full IDX universe (up to ~956 stocks)
+    - all_idx=False: LQ45 core list (26 stocks)
     """
     if all_idx:
         return fetch_all_idx_tickers(max_count=limit)
-    from config import IDX_STOCKS
     return list(IDX_STOCKS)
